a9f8a98e7dfb382f93ba5504a3172125b93ed5d8
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-06-05 23:15:31 +0900

    Initial Commit

---
+++ pythonsta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pythonsta
+//
+//  Created by 茂木史明 on 2026/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
